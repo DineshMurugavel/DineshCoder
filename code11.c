135c959387a6ea104cee8f19e2ec9e56816083a9
DineshMurugavel <30723921+DineshMurugavel@users.noreply.github.com>
2017-08-15 17:29:37 -1200

    Update code11.c

--- code11.c
+++ code11.c
@@ -3,10 +3,8 @@ int main()
 {
     int base, exponent;
     long long result = 1;
-    printf("Enter a base number: ");
     scanf("%d", &base);
 
-    printf("Enter an exponent: ");
     scanf("%d", &exponent);
     while (exponent != 0)
     {
